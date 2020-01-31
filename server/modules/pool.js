7086e73e898cf90e3a0ff5aa60fcf3ef5ab5f3e0
Allen Lucke <allenlucke@gmail.com>
2020-01-31 14:00:59 -0600

    added docker to codebase

---
+++ server/modules/pool.js
@@ -0,0 +1,21 @@
+const pg = require('pg');
+const Pool = pg.Pool;
+const pool = new Pool ({
+    database: process.env.POSTGRES_DB ? process.env.POSTGRES_DB : "koala_holla",
+    host: process.env.POSTGRES_HOST ? process.env.POSTGRES_HOST : "localhost",
+    user: process.env.POSTGRES_USER ? process.env.POSTGRES_USER : 'postgres',
+    password: process.env.POSTGRES_PASSWORD ? process.env.POSTGRES_PASSWORD: 'postgres',
+    port: process.env.PORT_DB ? process.env.PORT_DB : 5432,
+    max: 10,
+    idleTimeoutMillis: 30000
+});
+
+pool.on('connect', ()=>{
+    console.log('connected!');
+});
+
+pool.on('error', (error) => {
+    console.log(`err:  ${error}`);
+});
+
+module.exports = pool;
---
+++ start-data.sql
@@ -0,0 +1,4 @@
+INSERT INTO "koalas" ("name", "gender", "age", "ready_to_transfer", "notes")
+VALUES ('Tom', 'Male', 12, FALSE, 'Angry Koala'),
+('Jane', 'Female', 8, FALSE, 'Tired and hungry'),
+('Jeff', 'Male', 4, FALSE, 'Likes apples');
